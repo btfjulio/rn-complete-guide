8d85eada03c9c1880a0453dd3e597aa70de5db5e
Bruno Tostes <btfjulio@hotmail.com>
2020-02-19 11:46:55 -0300

    create react native app

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+
+export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+  
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+  }
+  
+  return (
+    <View>
+      <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Course Goal"
+            style={styles.button}
+            onChangeText={goalInputHandler}
+            value={enteredGoal}
+          />
+          <Button title="ADD" onPress={addGoalHandler} />
+        </View>
+        <ScrollView>
+          {courseGoals.map(goal => (
+            <View key={goal} style={styles.listItem}>
+              <Text>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+  },
+  button: {
+    width: '80%', borderColor: 'black', borderWidth: 1 
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 10
+  }
+});
